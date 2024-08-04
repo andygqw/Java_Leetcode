249c5ae0d384d8274301a097f87d93d3a7a1407a
andygqw <andygqwang@gmail.com>
2024-08-03 18:49:32 -0700

    Added question 73

---
+++ src/N73_SetMatrixZeros.java
@@ -0,0 +1,16 @@
+public class N73_SetMatrixZeros implements Solution{
+
+    @Override
+    public void test(){
+
+
+
+    }
+
+    private void setZeros(int[][] matrix){
+
+
+
+
+    }
+}
--- src/ThreadPool.java
+++ src/ThreadPool.java
@@ -4,6 +4,8 @@ public class ThreadPool {
 
     public static void main(String[] args) {
 
+        long startTime = System.nanoTime();
+
         CompletableFuture.supplyAsync(() -> {
                     try {
                         // Simulate a long-running task
@@ -18,7 +20,7 @@ public class ThreadPool {
         CompletableFuture<String> future = CompletableFuture.supplyAsync(() -> {
                     try {
                         // Simulate a long-running task
-                        Thread.sleep(1000);
+                        Thread.sleep(4000);
                         return "Hello2";
                     } catch (InterruptedException e) {
                         throw new IllegalStateException(e);
@@ -30,6 +32,10 @@ public class ThreadPool {
 
         System.out.println(future.join());
 
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        System.out.println("Time Consumed: " + (duration / 1_000_000) + "ms");
         // Keep the main thread alive to see the async result
 //        try {
 //            Thread.sleep(3000);
